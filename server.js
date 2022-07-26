5994f2d4455718511f29d70341b0ca51c6956402
Kristina Faye <kglinoga@gmail.com>
2022-07-26 15:27:26 -0700

    added the POST code, no errors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { get } = require('http');
 // this allows us to use the path method for creating/linking to new files
 const path = require('path');
 const app = express();
@@ -37,10 +38,37 @@ app.get('/api/notes', (req,res)=>{
 });
 
 // POST /api/notes should rec a new note to save on the req body, add to the db.json file, and return the new note to client ***each note needs a unique id when saved (there are npm pkgs for this??)
-
+// POST request to add a review
+// NOTE: Data persistence isn't set up yet, so this will only exist in memory until we implement it
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
+  
+    // Destructuring assignment for the items in req.body
+    const { noteTitle, noteText } = req.body;
+  
+    // If all the required properties are present
+    if (noteTitle && noteText) {
+      // Variable for the object we will save
+      const addNote = {
+        noteTitle,
+        noteText,
+      };
+  
+      const response = {
+        status: 'success',
+        body: addNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+  });
 
 
 // boilerplate listener?? LOl we added stuff and it does NOT work 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}/note-taker/develop/public/index.html`)
+  console.log(`Example app listening at http://localhost:${PORT}`)
 );
